21098f2baa6aa3c07c41babf50e6295e88c5124b
steve <stev8855@gmail.com>
2024-07-14 00:34:09 -0500

    Refactor TreesView delete_node method to handle non-existent node error

--- views/trees_view.py
+++ views/trees_view.py
@@ -80,8 +80,7 @@ class TreesView(QWidget):
     def delete_node(self):
         value, ok = QInputDialog.getInt(self, "Eliminar Nodo", "Ingrese el valor del nodo a eliminar:")
         if ok:
-            # TODO Verificar que recorra el arbol correctamente para encontrar el nodo y si no lo encuentra, mostrar un mensaje de error 
-            if value not in self.tree.values: 
+            if not self.find_node(self.tree.root, value):
                 self.show_error(f"Error: El nodo con valor {value} no existe")
                 return
             self.tree.delete_node(value, self)
