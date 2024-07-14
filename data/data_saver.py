0850bbe15c83172782e159f16c848fd0c86985da
steve <stev8855@gmail.com>
2024-07-14 00:23:06 -0500

    feat: Update data_saver.py to save data as Excel file

--- data/data_saver.py
+++ data/data_saver.py
@@ -1,11 +1,13 @@
 from PyQt6.QtWidgets import QFileDialog
 from PyQt6.QtGui import QPixmap
+import pandas as pd
 
 def save_sorted_data(data):
-    file_name, _ = QFileDialog.getSaveFileName(None, 'Guardar Archivo', '', 'Archivos de Texto (*.txt)')
+    file_name, _ = QFileDialog.getSaveFileName(None, 'Guardar Archivo', '', 'Archivos de Excel (*.xlsx)')
     if file_name:
-        with open(file_name, 'w') as file:
-            file.write(data)
+        data_list = [item.strip() for item in data.split(',') if item.strip()]
+        df = pd.DataFrame(data_list, columns=['Números'])
+        df.to_excel(file_name, index=False)
 
 def save_screenshot(window):
     file_name, _ = QFileDialog.getSaveFileName(None, 'Guardar Captura de Pantalla', '', 'Imágenes (*.png *.jpg *.bmp)')
